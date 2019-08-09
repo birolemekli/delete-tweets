1e860a0841f53744da84b990ef1ce5cc5f21941c
Birol Emekli <birol.emekli@hotmail.com>
2019-08-09 15:54:18 +0300

    delete tweets

---
+++ delete_tweets.py
@@ -0,0 +1,40 @@
+
+# -*- coding: utf-8 -*-
+
+import tweepy
+
+CONSUMER_KEY= "****"
+CONSUMER_SECRET="****"
+ACCESS_TOKEN="****"
+ACCESS_TOKEN_SECRET="****"
+
+#%% Api connection
+
+def auth_login(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+#%%
+def getPost(api,username):
+    for status in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended").items():
+        post.append(status)
+
+#%%
+def deletePost(year):
+    for i in range(len(app)):
+        if app[i].created_at.year<year:
+            api.destroy_status(id=app[i].id)
+            
+#%%
+if __name__=="__main__":
+    app=[]
+    username="@username"
+    year=1
+    api=auth_login(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+    getPost(api,username)
+    deletePost(year)
+    
+    
+
